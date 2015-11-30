6410e0493cdc71fc8eb695e5be7d8dbdb60305df
XSVOI <guicai.li.china@gmail.com>
2015-11-30 10:54:52 +0800

    init

---
+++ DynamicsDemo/DynamicsDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DynamicsDemo
+//
+//  Created by 力贵才 on 15/11/30.
+//  Copyright © 2015年 Guicai.Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

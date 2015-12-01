e72238ec795d4e7da19b722dea6571dacaea22f4
XSVOI <guicai.li.china@gmail.com>
2015-12-01 10:43:35 +0800

    滤镜效果

--- CoreImageDemo/CoreImageDemo/ViewController.swift
+++ CoreImageDemo/CoreImageDemo/ViewController.swift
@@ -9,10 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    /*
+        CIFilter 滤镜
+    */
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let fileURL = NSBundle.mainBundle().URLForResource("image", withExtension: "png")
+        
+        let beginImage = CIImage(contentsOfURL: fileURL!)
+        
+        let filter = CIFilter(name: "CISepiaTone")!
+    
+        filter.setValue(beginImage, forKey: kCIInputImageKey)
+        
+        filter.setValue(0.5, forKey: kCIInputIntensityKey)
+        
+//        let newImage = UIImage(CIImage: filter.outputImage!)
+//        
+//        self.imageView.image = newImage
+        
+        let context = CIContext(options: nil)
+        
+        let cgimg = context.createCGImage(filter.outputImage!, fromRect: filter.outputImage!.extent)
+        
+        let newImage = UIImage(CGImage: cgimg)
+        
+        self.imageView.image = newImage
+        
     }
 
     override func didReceiveMemoryWarning() {

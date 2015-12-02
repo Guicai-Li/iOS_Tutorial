881b61ffe2258f03d21c4ff667406f9408f9f269
XSVOI <guicai.li.china@gmail.com>
2015-12-02 10:54:47 +0800

    CounterView

---
+++ Flo/Flo/CounterView.swift
@@ -0,0 +1,49 @@
+//
+//  CounterView.swift
+//  Flo
+//
+//  Created by 力贵才 on 15/12/2.
+//  Copyright © 2015年 Guicai.Li. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let NoOfGlasses = 8
+let π:CGFloat = CGFloat(M_PI)
+
+@IBDesignable class CounterView: UIView {
+    
+    @IBInspectable var counter: Int = 5
+    @IBInspectable var outlineColor: UIColor = UIColor.blueColor()
+    @IBInspectable var counterColor: UIColor = UIColor.orangeColor()
+    
+    override func drawRect(rect: CGRect) {
+        // 1
+        let center = CGPoint(x:bounds.width/2, y: bounds.height/2)
+        
+        // 2
+        let radius: CGFloat = max(bounds.width, bounds.height)
+        
+        // 3
+        let arcWidth: CGFloat = 76
+        
+        // 4
+        let startAngle: CGFloat = 3 * π / 4
+        let endAngle: CGFloat = π / 4
+        
+        // 5
+        var path = UIBezierPath(arcCenter: center,
+            radius: radius/2 - arcWidth/2,
+            startAngle: startAngle,
+            endAngle: endAngle,
+            clockwise: true)
+        
+        // 6
+        path.lineWidth = arcWidth
+        counterColor.setStroke()
+        path.stroke()
+
+    }
+}
+
